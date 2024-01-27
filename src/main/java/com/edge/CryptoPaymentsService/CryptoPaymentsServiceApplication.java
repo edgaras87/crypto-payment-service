f796e2de55dd88eb51a0189b941024b3ac22eb9b
Your Name <you@example.com>
2024-01-27 11:15:48 -0500

    Initialize project.

---
+++ src/main/java/com/edge/CryptoPaymentsService/CryptoPaymentsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.edge.CryptoPaymentsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPaymentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoPaymentsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edge/CryptoPaymentsService/CryptoPaymentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edge.CryptoPaymentsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPaymentsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
